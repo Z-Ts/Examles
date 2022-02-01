3ac46b493da98c2074072a2985b163569e2efe36
user <git.test@inbox.ru>
2022-02-01 13:26:02 +0300

    Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next (1,5);
+Console.WriteLine (numberA);
+int numberB = new Random().Next (6,10);
+Console.WriteLine (numberB);
+int resulte = numberA + numberB;
+Console.WriteLine (resulte);
